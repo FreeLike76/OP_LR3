d9465259c898bbbc55b232b5e49157b746449c52
Dmytro Geleshko <dimahunter17@gmail.com>
2020-03-26 16:41:30 +0100

    Testing Hash function

--- OP_LR3.cpp
+++ OP_LR3.cpp
@@ -5,9 +5,34 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#define HSIZE 200000
 
+
+using namespace std;
+
+int getHash(string a)
+{
+	unsigned long hash = 5381;
+	int c;
+
+	for (int i = 0; i < a.length(); i++)
+	{
+		c = a[i];
+		hash = ((hash << 5) + hash) + c;
+	}
+	return hash%HSIZE;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string name;
+	do
+	{
+		cin >> name;
+		cout << getHash(name)<<endl;
+		
+	} while (name != "quit");
+	int* arr = new int[HSIZE];
+	cin >> name;
+	delete[] arr;
 }

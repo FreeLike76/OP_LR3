6d9333ea9531a5e877da691ebe5362b0eb53b6ed
Dmytro Geleshko <dimahunter17@gmail.com>
2020-03-27 13:18:29 +0100

    class Node=done, class List=wip

--- OP_LR3.cpp
+++ OP_LR3.cpp
@@ -5,10 +5,52 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#define HSIZE 200000
+#define HSIZE 100000
 
 
 using namespace std;
+
+class strList
+{
+	class Node
+	{
+	public:
+		Node* pNext;
+		string word;
+		string data;
+		Node(string word,string data,Node* pNext=nullptr)
+		{
+			this->word = word;
+			this->data = data;
+			this->pNext = pNext;
+		}
+
+	};
+	int lsize;
+	Node* head;
+public:
+	strList()
+	{
+		lsize = 0;
+		head = nullptr;
+	}
+	void push_back(string word,string data)
+	{
+		if (head==nullptr)
+		{
+			head = new Node(word, data);
+		}
+		else
+		{
+			Node* current = this->head;
+			while (current->pNext!=nullptr)
+			{
+				current = current->pNext;
+			}
+			current->pNext = new Node(word, data);
+		}
+	}
+};
 
 int getHash(string a)
 {
@@ -25,14 +67,7 @@ int getHash(string a)
 
 int main()
 {
-	string name;
-	do
-	{
-		cin >> name;
-		cout << getHash(name)<<endl;
-		
-	} while (name != "quit");
-	int* arr = new int[HSIZE];
-	cin >> name;
-	delete[] arr;
+	strList a;
+	a.push_back("Hash", "My work");
+	a.push_back("look", "at this");
 }

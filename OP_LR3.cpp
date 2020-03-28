e90bed4bd993649e81169c57043647179d481362
Dmytro Geleshko <dimahunter17@gmail.com>
2020-03-28 21:58:54 +0100

    Organised prototypes

--- OP_LR3.cpp
+++ OP_LR3.cpp
@@ -29,47 +29,17 @@ class strList
 	int lsize;
 	Node* head;
 public:
-	strList()
-	{
-		lsize = 0;
-		head = nullptr;
-	}
-	~strList() {};
-	int getlsize() 
-	{
-		return lsize;
-	};
-	void push_back(string word,string data)
-	{
-		if (head==nullptr)
-		{
-			head = new Node(word, data);
-		}
-		else
-		{
-			Node* current = this->head;
-			while (current->pNext!=nullptr)
-			{
-				current = current->pNext;
-			}
-			current->pNext = new Node(word, data);
-		}
-		lsize++;
-	}
-	string operator[](const string find)
-	{
-		Node* current = this->head;
-		while (current->pNext != nullptr)
-		{
-			if (current->word==find)
-			{
-				return current->data;
-			}
-			current = current->pNext;
-		}
-		return "Can't find required data! Maybe you misspelled the word or this vocabulary is missing that word. Please try again.\n";
-	}
+	strList();
+	~strList();
+	int getlsize();
+	void push_back(string word, string data);
+	void pop_front();
+	string operator[](const string find);
 };
+
+
+
+
 
 int getHash(string a)
 {
@@ -93,4 +63,60 @@ int main()
 	a.push_back("look", "at this");
 	cout << a.getlsize();
 	cout << a["Hash"];
+}
+
+strList::strList()
+{
+
+	lsize = 0;
+	head = nullptr;
+
+}
+
+strList::~strList()
+{
+}
+
+int strList::getlsize()
+{
+	return lsize;
+}
+
+void strList::push_back(string word, string data)
+{
+
+
+	if (head == nullptr)
+	{
+		head = new Node(word, data);
+	}
+	else
+	{
+		Node* current = this->head;
+		while (current->pNext != nullptr)
+		{
+			current = current->pNext;
+		}
+		current->pNext = new Node(word, data);
+	}
+	lsize++;
+
+}
+
+void strList::pop_front()
+{
+}
+
+string strList::operator[](const string find)
+{
+	Node* current = this->head;
+	while (current->pNext != nullptr)
+	{
+		if (current->word == find)
+		{
+			return current->data;
+		}
+		current = current->pNext;
+	}
+	return "Can't find required data! Maybe you misspelled the word or this vocabulary is missing this word. Please try again.\n";
 }
